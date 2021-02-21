6bb04fa7d3c5a0dd9ef944fd9bd6db310289782c
Enrique Nuñez <nenrique0112@gmail.com>
2021-02-21 11:16:16 -0400

    Decorators with args

--- decorators.py
+++ decorators.py
@@ -1,3 +1,4 @@
+# reference: https://realpython.com/primer-on-python-decorators/
 
 # Decorator functions with args must always return a value
 # in order to get the results of the decorated function
@@ -28,12 +29,27 @@ def greetings(func_as_param):
 def salute_person(name):
     print(f'Hello {name}')
 
+# Decorators with args
+def repeat(num_times):
+    def dec(func):
+        def wrapper(*a, **kw):
+            for i in range(num_times):
+                func(*a, **kw)
+        return wrapper
+    return dec
+
+@repeat(num_times = 0)
+def greet(name):
+    print(f'Hello {name}')
+
 def main():
     a = int(input("A: "))
     b = int(input("B: "))
     print(addition(a, b))
     print(substraction(a, b))
     salute_person("Constantina Guess")
+    print('--------------------------')
+    greet('Enrique')
 
 if __name__ == "__main__":
     main() 
