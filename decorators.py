ab87d834d95204df397def6bf8185cd6cf75a5c2
Enrique Nuñez <nenrique0112@gmail.com>
2021-02-21 01:42:57 -0400

    Added more functions with its decorators

--- decorators.py
+++ decorators.py
@@ -18,11 +18,22 @@ def addition(a: int, b: int):
 def substraction(a: int, b: int):
     return a - b
 
+def greetings(func_as_param):
+    def internal_func(*args, **kwargs):
+        func_as_param(*args, **kwargs)
+        print("How are you today?")
+    return internal_func
+
+@greetings
+def salute_person(name):
+    print(f'Hello {name}')
+
 def main():
     a = int(input("A: "))
     b = int(input("B: "))
     print(addition(a, b))
     print(substraction(a, b))
+    salute_person("Constantina Guess")
 
 if __name__ == "__main__":
     main() 
