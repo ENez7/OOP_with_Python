99debe7b03d16880f3832a8b14846401d7f46bec
Enrique Núñez <nenrique0112@gmail.com>
2021-02-21 23:58:35 -0400

    Dont know what to write

--- classPractice.py
+++ classPractice.py
@@ -3,16 +3,12 @@ class Person:
         self._name = name
         self._edad = edad
     
-    def saludar(self):
+    def greetings(self):
         print(f"Hola {self._name}, yo soy un bot")
 
 def main():
-    #person = Person("Enrique", 20)
-    #person.greetings()
-    a = int(input("A: "))
-    b = int(input("B: "))
-    print(addition(a, b))
-    print(substraction(a, b))
+    person = Person("Enrique", 20)
+    person.greetings()
 
 if __name__ == "__main__":
     main() 
